13142f3ed58dc7709ba740114205e3da7a1e6415
kzhein <kyawzinhein6@gmail.com>
2022-06-14 16:42:42 +0630

    Remove cors package

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 
 let subscribers = {};
@@ -31,7 +30,6 @@ const publish = message => {
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/messages', (req, res) => {
